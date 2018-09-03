aea2b63a9762dba0815764da0e07b9ef6fc3e520
Till Born <nogiax@googlemail.com>
2018-09-03 22:53:36 +0200

    initial backend commit

---
+++ backend/src/main/groovy/com/bornium/boostrappingascode/BoostrappingAsCodeApplication.groovy
@@ -0,0 +1,12 @@
+package com.bornium.boostrappingascode
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BoostrappingAsCodeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BoostrappingAsCodeApplication, args
+	}
+}
