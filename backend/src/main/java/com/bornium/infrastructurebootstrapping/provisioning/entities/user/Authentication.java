1b0ba72f570a8b92149e8282531dbb8566e46bee
Till Born <nogiax@googlemail.com>
2019-06-29 16:59:01 +0200

    fix authentication json

--- backend/src/main/java/com/bornium/infrastructurebootstrapping/provisioning/entities/user/Authentication.java
+++ backend/src/main/java/com/bornium/infrastructurebootstrapping/provisioning/entities/user/Authentication.java
@@ -1,11 +1,13 @@
 package com.bornium.infrastructurebootstrapping.provisioning.entities.user;
 
 import com.bornium.infrastructurebootstrapping.provisioning.entities.Base;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class Authentication extends Base {
     public Authentication(String id) {
         super(id);
     }
 
+    @JsonIgnore
     public abstract String getValue();
 }
